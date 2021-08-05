7abd978e278fc997c275b300e482b2fd027c385e
Steve-njeri <stevenjeri254@gmail.com>
2021-08-05 16:13:31 +0300

    modified files

--- src/test/java/models/TaskTest.java
+++ src/test/java/models/TaskTest.java
@@ -1,3 +1,4 @@
+
 package models;
 
 import org.junit.Test;
@@ -31,6 +32,6 @@ public class TaskTest {
 
     //helper methods
     public Task setupNewTask(){
-        return new Task("Mow the lawn");
+        return new Task("Mow the lawn", 1);
     }
 }
